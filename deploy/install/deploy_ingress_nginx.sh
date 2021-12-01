73ac24c4a431572daad786ff888df7bedfbeb0f1
Wei Y <83482178+w-yue@users.noreply.github.com>
2021-12-01 17:14:31 +0000

    1. use local ingress_nginx_deploy.yml instead of latest version to work for k8s 1.18.2; 2. change "postgres" to "postgresql" as "postgres" has been deprecated and no longer allowed.

--- deploy/install/deploy_ingress_nginx.sh
+++ deploy/install/deploy_ingress_nginx.sh
@@ -10,7 +10,8 @@
 set -o errexit
 
 echo "Create Nginx Ingress Controller..."
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml >/dev/null
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml >/dev/null
+kubectl apply -f ../etc/deployments/ingress_nginx_deploy.yml
 
 # Remove unnecessary validation through webhooks when new ingresses are added
 kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission &>/dev/null
